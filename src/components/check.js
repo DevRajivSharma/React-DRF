3654d2f191582eaa05b4a7774dde82895e0fe271
RajivSharma <113875786+DevRajivSharma@users.noreply.github.com>
2024-07-22 12:07:16 +0530

    Update check.js
    latest

--- src/components/check.js
+++ src/components/check.js
@@ -11,7 +11,7 @@ const Check = () => {
 
   const handleSubmit = async () => {
     const response = await fetch(`${process.env.REACT_APP_API_URL}/add_name`, {
-      method: 'PUT',
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
